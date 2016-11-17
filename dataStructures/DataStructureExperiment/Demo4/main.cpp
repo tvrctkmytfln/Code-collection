8c2979e14e0c5a48377c5ffca702ebaab82cdf28
h-j-13 <h.j.13.new@gmail.com>
2016-11-17 22:30:55 +0800

    16.11.17

--- dataStructures/DataStructureExperiment/Demo4/main.cpp
+++ dataStructures/DataStructureExperiment/Demo4/main.cpp
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <malloc.h>
 #include <list>
+#include <queue>
+#include <math.h>
 
 using namespace std;
 
@@ -12,7 +14,7 @@ using namespace std;
  * */
 
 // 图的遍历（默认按照无向图来写
-// 1.1 图的表示方法
+// 1.0 图的表示方法
 // 邻接表
 
 //　数据结构
@@ -400,12 +402,311 @@ void cityWaySolution()
 /** 解决城市问题 */
 {/*pri(Graph_A city);*/}
 
+// 3.1 图的拓扑排序
+// 有向图的构造
+void CreateALGraph(Graph* G)      // 构造一个图，简化了malloc判断
+/** 有向图的构造（邻接表 */
+{
+    // 获取定点和边数
+    int i,j,k;
+    int a;
+    EdgeNode *s;
+    printf("读入顶点数和边数：");
+    scanf("%d,%d",&G->Vertex_num,&G->Edge_num);
+    // 构建定点表
+    for (i=0;i<G->Vertex_num;i++)
+    {
+        fflush(stdin);  // 清空缓冲
+        printf("建立顶点表：");
+        cin>>G->adjlist[i].vertex;
+        //G->adjlist[i].vertex= (VertexType) getchar();
+        G->adjlist[i].firstedge=NULL;   // 第一条边指向空
+    }
+    // 构建边表
+    printf("建立边表\n");
+    for (k=0;k<G->Edge_num;k++)
+    {
+        printf("读入(vi-vj)的顶点对序号");
+        scanf("%d,%d",&i,&j);
+        // 只构造一条边
+        s=(EdgeNode*)malloc(sizeof(EdgeNode));
+        s->adjvex=j;    // 边指向j
+        s->next=G->adjlist[i].firstedge;  //插入到顶点i的边表（头插
+        G->adjlist[i].firstedge=s;
+//        s=(EdgeNode*)malloc(sizeof(EdgeNode));
+//        s->adjvex=i;
+//        s->next=G->adjlist[j].firstedge;
+//        G->adjlist[j].firstedge=s;
+    }
+}
 
-int main()
+// 有向图的拓扑排序
+// 思路：选择一个没有前驱的节点,删除这个节点和以这个节点为起点发出的边,重复这个过程
+//      维护一个顶点出度集来进行操作[不改变本身图的内容，也不用复制
+
+// Kahn算法的图拓扑排序
+void topSort_kahn(Graph* G)
+/** 拓扑排序Kahn算法 */
+{
+    EdgeNode *cur = nullptr;         // 临时用邻接表游标
+    queue<char> result;              // 拓扑排序结果队列
+    queue<int> temp;                 // 拓扑排序临时队列
+    int VertexIndegree[G->Vertex_num] = {0};    // 顶点入度集
+    // 初始化顶点入度集
+    for(int i=0;i<G->Vertex_num;i++)
+    {
+        cur = G->adjlist[i].firstedge;
+        while(cur)
+        {
+            VertexIndegree[cur->adjvex]++;
+            cur = cur->next;
+        }
+    }
+    // 循环查找
+    for(int j=0;j<G->Vertex_num;j++)
+        if(!VertexIndegree[j])  // 找到第一个入度为0的点
+        {
+            temp.push(j);
+            break;
+        }
+
+    if(temp.empty())
+    {// 特殊情况处理
+        cout<<"找不到入度为0的点"<<endl;
+        return;
+    }
+    while(!temp.empty())
+    {// 完成拓扑序列
+        int v = temp.front();   // 处理节点
+        VertexIndegree[v] = -1;     // 处理过的点标记为-1
+        temp.pop(); // 这个地方其实temp退化成一个数就可以了
+                    // 当然也可以每次把入度为0的点全部入队,
+                    // 然后再处理，两个思路的拓扑序列是不同的
+        result.push(G->adjlist[v].vertex);  // 加入到拓扑序列中
+        cur = G->adjlist[v].firstedge;
+        while(cur)
+        {// 去掉这个以这个节点为起点的边
+            VertexIndegree[cur->adjvex]--;
+            cur = cur->next;
+        }
+        for(int k=0;k<G->Vertex_num;k++)
+            if(VertexIndegree[k]==0)
+            {   // 一次处理一个就行了,否则会重复输出
+                temp.push(k);
+                break;
+            }   // 还有其他思路
+//        cout<<"\ntest：VertexIndegree ";
+//        for(int k=0;k<G->Vertex_num;k++)
+//            cout<<VertexIndegree[k]<<" ";
+    }
+    // 有环情况处理
+    for(int l=0;l<G->Vertex_num;l++)
+        if(VertexIndegree[l]!=-1)
+        {   // 这样少用了一个计数变量
+            cout<<"图中有环"<<endl;
+            return;
+        }
+    while(!result.empty())
+    {// 输出排序序列
+        cout<<result.front()<<"->";
+        result.pop();
+    }
+
+}
+
+void topSort_BFS(Graph* G)
+/** 拓扑排序Kahn算法 */
+{/** 按照广度优先的思路遍历图也可以完成拓扑排序,实现基本同BFS,代码略... */}
+
+// 3.2 图的拓扑排序 输出全部可能
+//      思路：关键是回朔~参考csdn
+//      网上参考～http://www.docin.com/p-680578799.html
+//      [这个以后看的时候可以再想想
+
+//      思路2:记录每个节点的层数，不光记录最大的层数，也记录最小的层数
+//            通过这个记录层数的容器来完成全部输出
+//            规则-当要输出节点时，所有最大层数小与此节点的最小层数的节点一定要已经输出
+//            以下是实现
+void topSort_ArrayBFS(Graph* G)
+/** 拓扑排序[输出所有可能 */
+{// 默认是个联通图且无环 !
+    EdgeNode *cur = nullptr;         // 临时用邻接表游标
+    queue<int> temp;                 // 拓扑排序临时队列
+    int depth_max = -1;
+    int VerterDepth[G->Edge_num][2] ={0};          // 定点深度数组 0-最小深度 1-最大深度
+    int VertexIndegree[G->Vertex_num] = {0};       // 顶点入度集
+    // 初始化顶点入度集
+    for (int i=0;i<G->Vertex_num;i++)
+    {
+        cur = G->adjlist[i].firstedge;
+        while (cur)
+        {
+            VertexIndegree[cur->adjvex]++;
+            cur = cur->next;
+        }
+    }
+    // 循环查找
+    for (int j=0;j<G->Vertex_num;j++)
+        if (!VertexIndegree[j])  // 找到第一个入度为0的点
+        {
+            VerterDepth[j][0] = 0;  // 起点设置最小深度为0
+            VerterDepth[j][1] = 0;  // 起点设置最大深度为0
+            temp.push(j);
+        }
+    while(!temp.empty())
+    {
+        int v = temp.front();   // 顶点序号
+        VertexIndegree[v] = -1; // 处理过这个定点
+        int min_depth = VerterDepth[v][0]; // 当前顶点最小深度
+        int max_depth = VerterDepth[v][1]; // 当前顶点最大深度
+        // cout<<"TEST：正在处理"<<G->adjlist[v].vertex<<endl;
+        temp.pop();
+        cur = G->adjlist[v].firstedge;
+        while(cur)
+        {// 更新这个定点所连接的定点的深度值
+            VertexIndegree[cur->adjvex]--;  // 更新入度表
+            // 更新深度值,（初始化||需要更改值）
+            if(VerterDepth[cur->adjvex][0]==0||VerterDepth[cur->adjvex][0]>(min_depth+1))
+                VerterDepth[cur->adjvex][0] = min_depth+1;
+            if(VerterDepth[cur->adjvex][1]==0||VerterDepth[cur->adjvex][1]<(max_depth+1))
+            {
+                VerterDepth[cur->adjvex][1] = max_depth+1;
+                if(VerterDepth[cur->adjvex][1]>depth_max)
+                    depth_max = VerterDepth[cur->adjvex][1];    // 更新最大层数记录变量
+            }
+            cur = cur->next;
+        }
+        for(int k=0;k<G->Vertex_num;k++)
+            if(VertexIndegree[k]==0)
+            {// 入度为0的定点入队
+                temp.push(k);
+                break;
+            }
+    }
+//    // just show
+//    for(int k=0;k<G->Vertex_num;k++)
+//    {
+//        cout<<"顶点 "<<G->adjlist[k].vertex<<
+//            " 的最小深度为: "<<VerterDepth[k][0]<<
+//            " 的最大深度为: "<<VerterDepth[k][1]<<endl;
+//    }
+    // 思路1：   0，按照最大层数排序输出，
+    //          1，同层需要遍历所有排序可能
+    //          2，输出之后，如果最大层大于最小层，则将最大层的数值-1
+    //          3，重复上述过程，直到所有最大层数等于最小层
+    // 代码略... 感觉这个好麻烦
+
+    // 思路2：根据最大最小层数输出序列[输出成一个字符串
+    int count = 0;  // 记录输出了多少的序列 - 层数
+    // cout<<"TEST:最大深度 "<<depth_max;
+    cout<<"note：‘|’之间的定点可以随意互换位置,带‘[]’标志的节点有且仅能输出一次，选择不同的位置将出现不同的拓扑序列"<<endl;
+    while(count <= depth_max)
+    {
+        for(int l=0;l<G->Vertex_num;l++)
+        {   // 输出序列
+            if(VerterDepth[l][0]==count&&VerterDepth[l][1]==count)
+                cout<<G->adjlist[l].vertex<<" ";
+            else if(VerterDepth[l][0]==count||VerterDepth[l][1]==count)
+                cout<<"["<<G->adjlist[l].vertex<<"] ";
+        }
+        cout<<"| ";
+        count ++;
+    }
+    cout<<endl<<"此图的所有拓扑序列输出完毕"<<endl;
+}
+
+// 3.3 图的拓扑排序[十字链表 ～ 参考http://blog.csdn.net/junbopengpeng/article/details/13020315
+// 图的十字链表描述
+#define MaxVex 20
+typedef int EdgeType ;     // 定义弧信息的类型
+typedef char VertexType ;  // 定义顶点的类型
+
+typedef struct EdgeNode_l  // 弧结点的定义
+{
+    int tailvex ;   // 弧尾结点的下标
+    int headvex ;   // 弧头结点的下标
+    struct EdgeNode_l* headlink ;  // 指向弧头相同的下一条弧的链域
+    struct EdgeNode_l * taillink ;  // 指向弧尾相同的下一条弧的链域
+    EdgeType info ; // 弧中所含的信息
+} EdgeNode_l ;
+
+typedef struct VertexNode_l  // 顶点结点的定义
+{
+    int index ;  // 下标值
+    VertexType data  ;  // 顶点内容
+    EdgeNode_l* firstout ; // 顶点的第一条出弧
+    EdgeNode_l* firstin ;  // 顶点的第一条入弧
+} VertexNode_l ;
+
+typedef struct OLGraph   // 十字链表存储结构的有向图定义
+{
+    VertexNode_l vertices[MaxVex] ;
+    int vexnum ;    // 边数
+    int arcnum ;    // 弧数
+} OLGraph ;
+
+// 图的十字链表描述的构造
+void CreateGraph_L(OLGraph* G)
+/** 图的十字链表描述的构造 */
+{
+    cout << "输入弧的数目与顶点的数目:";
+    cin >> G->arcnum;
+    cin >> G->vexnum;
+    // 输入顶点的信息
+    printf("请输入顶点信息:");
+    for (int i = 0; i < G->vexnum; i++) {
+        scanf("%c", &G->vertices[i].data);
+        G->vertices[i].index = i;
+        G->vertices[i].firstin = NULL;
+        G->vertices[i].firstout = NULL;
+    }
+    // 输入弧的信息
+    printf("请输入边的信息:");
+    for (int j = 0; j < G->arcnum; j++) {
+        EdgeNode_l *edge = (EdgeNode_l *) malloc(sizeof(EdgeNode_l));
+        edge->taillink = NULL;
+        edge->headlink = NULL;
+        scanf("%d %d", &edge->tailvex, &edge->headvex);
+        // 将该弧插入到当前顶点的出弧中
+        EdgeNode_l *p = G->vertices[edge->tailvex].firstout;
+        if (p == NULL)  // 第一次插入边结点
+            G->vertices[edge->tailvex].firstout = edge;
+        else {
+            while (p != NULL) {
+                if (p->taillink == NULL)
+                    break;
+                p = p->taillink;
+            }
+            p->taillink = edge;
+        }
+        // 将该弧插入到当前顶点的入弧中
+        EdgeNode_l *q = G->vertices[edge->headvex].firstin;
+        if (q == NULL)
+            G->vertices[edge->headvex].firstin = edge;
+        else {
+            while (q->headlink != NULL) {
+                if (q->headlink == NULL)
+                    break;
+                q = q->headlink;
+            }
+            q->headlink = edge;
+        }
+    }
+}
+
+// 3.3 图[十字链表描述]拓扑排序
+// @override
+void topSort_kahn(OLGraph* G)
+/** 图[十字链表描述]拓扑排序 */
+{/*思路与邻接表相同,依然是kahn算法,利用边链表初始化存储和更新顶点入度集即可,与上面的重载基本相同*/}
+
+int main(void)
 {
     // 初始化图的时候需要先为变量申请内存空间
-    Graph_A* G= (Graph_A*)malloc(sizeof(Graph_A));
-    CreateGraph_A(G);
-    GraphPrim(G);
-    return 0;
+    // demo
+//    Graph* G= (Graph*)malloc(sizeof(Graph));
+//    CreateALGraph(G);
+//    topSort_ArrayBFS(G);
+
+    cout<<"author：`13,以上代码使用 gcc 全部编译测试通过,IDE：Clion -2016.11.17"<<endl;
 }
